ed09fcb5250f2464ef09815322b34462f8a49508
Dario Crippa <61752576+AstroWLAN@users.noreply.github.com>
2022-11-14 20:49:25 +0100

    Initial Commit

---
+++ Healthub/HealthubApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthubApp.swift
+//  Healthub
+//
+//  Created by Dario Crippa on 14/11/22.
+//
+
+import SwiftUI
+
+@main
+struct HealthubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
