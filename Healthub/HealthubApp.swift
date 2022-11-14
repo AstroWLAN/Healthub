e84aec838e3b837cb8c816bbdd573ef6943260c6
Dario Crippa <61752576+AstroWLAN@users.noreply.github.com>
2022-11-15 00:21:31 +0100

    Adds custom label style

--- Healthub/HealthubApp.swift
+++ Healthub/HealthubApp.swift
@@ -1,5 +1,27 @@
 import SwiftUI
 
+/// **Healthub Label Style**
+// Custom label inspired by the iOS settings app
+struct HealthubLabel : LabelStyle {
+    
+    var labelColor : Color
+    
+    func makeBody(configuration: Configuration) -> some View {
+        Label {
+            configuration.title
+        } icon: {
+            configuration.icon
+                .font(.system(size: 17))
+                .foregroundColor(.white)
+                .background(
+                    RoundedRectangle(cornerRadius: 7)
+                        .foregroundColor(labelColor)
+                        .frame(width: 30,height: 30)
+                )
+        }
+    }
+}
+
 @main
 struct HealthubApp: App {
     var body: some Scene {
