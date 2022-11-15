8f85c7ff7db8b4809de2040a50a243ed8df96d0e
Dario Crippa <61752576+AstroWLAN@users.noreply.github.com>
2022-11-15 01:25:03 +0100

    Customises some View aspects and defines new custom views for the app

--- Healthub/HealthubApp.swift
+++ Healthub/HealthubApp.swift
@@ -1,5 +1,22 @@
 import SwiftUI
 
+// Adds the capability of removing the view from a view stack
+extension View {
+    
+    @ViewBuilder func isVisible(_ remove: Bool = false) -> some View {
+        if remove { }
+        else { self }
+    }
+}
+
+// Removes the back button text
+extension UINavigationController {
+    
+    open override func viewWillLayoutSubviews() {
+        navigationBar.topItem?.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+    }
+}
+
 /// **Healthub Label Style**
 // Custom label inspired by the iOS settings app
 struct HealthubLabel : LabelStyle {
@@ -18,6 +35,58 @@ struct HealthubLabel : LabelStyle {
                         .foregroundColor(labelColor)
                         .frame(width: 30,height: 30)
                 )
+        }
+    }
+}
+
+/// **Unit Measure View**
+// A view that represents a unit of measure
+struct UnitMeasureView : View {
+    
+    let unitMeasure : String
+    
+    var body : some View {
+        Text(unitMeasure)
+            .font(.system(size: 13,weight: .semibold))
+            .foregroundColor(Color(.systemGray))
+            .padding([.leading,.trailing],14)
+            .padding([.top,.bottom],4)
+            .background(
+                Capsule().foregroundColor(Color(.systemGray5))
+            )
+    }
+}
+
+
+/// **RecordTextfield**
+// A truly fancy custom textfield used to collect medical records and user data
+struct RecordTextfield : View {
+    
+    enum TextfieldType {
+        case personName
+    }
+    
+    @Binding var textVariable : String
+    
+    let glyph : String
+    let glyphColor : Color
+    let placeholder : String
+    let textfieldType : TextfieldType
+    let badInput : Bool
+    let measure : String
+    
+    var body : some View {
+        HStack{
+            Label(String(), systemImage: glyph).labelStyle(HealthubLabel(labelColor: glyphColor))
+            TextField(placeholder, text: $textVariable)
+                .offset(x: -8)
+            Spacer()
+            Image(systemName: "exclamationmark.circle.fill")
+                .font(.system(size: 17))
+                .foregroundColor(Color(.systemRed))
+                .opacity(badInput ? 1 : 0)
+            UnitMeasureView(unitMeasure: measure)
+                .isVisible(measure.isEmpty ? true : false)
         }
     }
 }
