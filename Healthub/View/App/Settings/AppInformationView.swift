a0b529890b1d1d8eb5fbdb7c292a4a7553e47c6c
Dario Crippa <61752576+AstroWLAN@users.noreply.github.com>
2022-11-17 00:47:57 +0100

    Adds AppInformation View

--- Healthub/View/App/Settings/AppInformationView.swift
+++ Healthub/View/App/Settings/AppInformationView.swift
@@ -1,9 +1,62 @@
 import SwiftUI
 
+// JSON data structure for AppInformation.json
+struct AppInformationJSON : Decodable, Identifiable {
+    let id : UUID
+    var information : String
+    
+    enum CodingKeys: CodingKey {
+        case id
+        case information
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = UUID()
+        self.information = try container.decode(String.self, forKey: .information)
+    }
+}
+
 struct AppInformationView: View {
+    
+    @State private var appInformation : [AppInformationJSON] = []
+    
+    // Decodes the AppInformation.json and creates an array of swift objects
+    private func readAppInformation(){
+        let jsonDecoder = JSONDecoder()
+        guard let dataURL = Bundle.main.url(forResource: "AppInformation", withExtension: "json"),
+              let fileData = try? Data(contentsOf: dataURL),
+              let jsonData = try? jsonDecoder.decode([AppInformationJSON].self, from: fileData)
+        else {
+            print("🔥 Something went wrong with the JSON decoding process")
+            return
+        }
+        appInformation = jsonData
+    }
+    
     var body: some View {
-        Text("Hello, Information 📋!")
-            .bold()
+        VStack(spacing: 20){
+            Image("RobotDraw")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150)
+                .opacity(0.3)
+            Text("Healthub")
+                .foregroundColor(Color(.systemGray))
+                .font(.system(.title2, weight: .bold))
+            
+            // App information descriptions
+            VStack(spacing: 10){
+                ForEach(appInformation) { field in
+                    Text(LocalizedStringKey(field.information))
+                        .font(.system(size: 13))
+                        .foregroundColor(Color(.systemGray2))
+                }
+                .multilineTextAlignment(.center)
+            }
+            .navigationBarTitle("Information", displayMode: .inline)
+        }
+        .onAppear(perform: readAppInformation)
     }
 }
 
