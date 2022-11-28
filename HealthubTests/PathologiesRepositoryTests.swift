3aa34d7360815f63a8796a2692d4f4fa4560e9af
Giovanni Dispoto <giovanni.dispoto@mail.polimi.it>
2022-11-28 18:59:03 +0100

    Add PathologiesRepositoryTests file

---
+++ HealthubTests/PathologiesRepositoryTests.swift
@@ -0,0 +1,22 @@
+//
+//  PathologiesRepositoryTests.swift
+//  HealthubTests
+//
+//  Created by Giovanni Dispoto on 28/11/22.
+//
+
+import XCTest
+
+final class PathologiesRepositoryTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    
+
+}
