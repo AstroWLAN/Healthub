eebb01515e732836c1706c470a8873aeb16baed4
Giovanni Dispoto <giovanni.dispoto@mail.polimi.it>
2022-12-01 15:13:11 +0100

    Code format

--- Healthub/Repositories/RepositoriesProtocol/PathologyRepositoryProtocol.swift
+++ Healthub/Repositories/RepositoriesProtocol/PathologyRepositoryProtocol.swift
@@ -9,10 +9,7 @@ import Foundation
 
 protocol PathologyRepositoryProcotol
 {
-   // typealias T = Pathology
     func add(pathologyName:String, completionHandler: @escaping (Bool?, Error?) -> Void)
-    
     func delete(pathologyId:Int, completionHandler: @escaping (Bool?, Error?) -> Void)
-    
     func getAll(completionHandler: @escaping ([Pathology]?, Error?) -> Void)
 }
