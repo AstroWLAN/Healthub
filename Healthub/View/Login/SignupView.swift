920fce6df613355883b21900b31f8a1938ca8cbd
AstroWLAN <dario2.crippa@mail.polimi.it>
2022-12-02 05:00:36 +0100

    Updates SignupView

--- Healthub/View/Login/SignupView.swift
+++ Healthub/View/Login/SignupView.swift
@@ -1,71 +1,69 @@
-//
-//  SignupView.swift
-//  Healthub
-//
-//  Created by Dario Crippa on 11/11/22.
-//
-
 import SwiftUI
+import AlertToast
 
 struct SignupView: View {
     
-    @State private var email : String = ""
-    @State private var password : String = ""
-    @State private var paswordCheck : String = ""
+    @State private var email : String = String()
+    @State private var password : String = String()
+    @State private var paswordCheck : String = String()
+    @State private var isPerformingSignup : Bool = false
     
     var body: some View {
-        VStack{
-            
-            // Illustration
-            Spacer()
-            Image("SignupDraw")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 150)
-            
-            // Text
-            VStack(spacing: 8){
-                Text("Welcome").font(.largeTitle).bold()
-                Text("Let's get in touch").foregroundColor(Color(.systemGray3))
-                .font(.system(size: 17)).bold()
-            }
-            .padding([.top],20)
-            
-            // "Continue with Email" button
-            Spacer()
+        NavigationStack{
             VStack{
-                TextField("\(Image(systemName: "envelope")) Email", text: $email)
-                    .frame(width: 300)
-                    .padding([.bottom],15)
-                SecureField("\(Image(systemName: "lock")) Password", text: $password)
-                    .frame(width: 300)
-                TextField("\(Image(systemName: "lock.rectangle.on.rectangle")) Repeat Password", text: $paswordCheck)
-                    .frame(width: 300)
-                    .padding([.bottom],15)
+                Spacer()
+                Image("SignupDraw")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150)
+                Text("Welcome")
+                    .font(.largeTitle.bold())
+                Text("Let's Get in Touch")
+                    .foregroundColor(Color(.systemGray))
+                    .font(.system(size: 17, weight: .bold))
+                Spacer()
+                VStack{
+                    TextField("\(Image(systemName: "envelope")) Email", text: $email)
+                        .padding(.bottom, 10)
+                        .textContentType(.username)
+                        .textInputAutocapitalization(.never)
+                        .keyboardType(.emailAddress)
+                    SecureField("\(Image(systemName: "lock")) Password", text: $password)
+                        .padding(.bottom, 10)
+                        .textContentType(.password)
+                    SecureField("\(Image(systemName: "lock.rectangle.on.rectangle")) Repeat Password", text: $paswordCheck)
+                        .textContentType(.password)
+                }
+                .autocorrectionDisabled(true)
+                .foregroundColor(Color(.systemGray))
+                .multilineTextAlignment(.center)
+                .frame(width: 300)
+                .padding(.vertical, 30)
+                Spacer()
+                Button(action: {
+                    isPerformingSignup = true
+                    // Signup method
+                    isPerformingSignup = false
+                },
+                       label: {
+                    Text("Signup")
+                        .font(.system(size: 15, weight: .semibold))
+                        .padding(EdgeInsets(top: 5, leading: 20, bottom: 5, trailing: 20))
+                })
+                .buttonStyle(.borderedProminent)
+                .padding(.bottom,40)
+                // .disabled() button logic ( empty fields, password mismatch etc... )
+                /*
+                 Displays signup errors
+                 .toast(isPresenting: SignupModelError, duration: 3){
+                     AlertToast(displayMode: .hud, type: .systemImage("exclamationmark.circle.fill", Color("HealthGray3")),title: "Signup Failure")
+                 }
+                */
             }
-            .foregroundColor(Color(.systemGray))
-            .multilineTextAlignment(.center)
-            .padding(.bottom,80)
-            
-            Spacer()
-            Button(action: { /* login action */ },
-                   label: {
-                Text("Signup")
-                    .font(.system(size: 15))
-                    .bold()
-                    .foregroundColor(Color(.white))
-                    .padding([.leading,.trailing],34)
-                    .padding([.top,.bottom],12)
-                    .background(RoundedRectangle(cornerRadius: 10).foregroundColor(Color(.systemPink)))
-            })
-            .padding(.bottom,20)
-            
         }
-    }
-}
-
-struct SignupView_Previews: PreviewProvider {
-    static var previews: some View {
-        SignupView()
+        .toolbar{
+            ProgressView().progressViewStyle(.circular)
+                .opacity(isPerformingSignup ? 1 : 0)
+        }
     }
 }
