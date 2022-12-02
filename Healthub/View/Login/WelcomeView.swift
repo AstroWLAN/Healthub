f7842429f3fd46676f6ec043f349dc1b056cb0be
AstroWLAN <dario2.crippa@mail.polimi.it>
2022-12-02 04:40:52 +0100

    Updates WelcomeView

--- Healthub/View/Login/WelcomeView.swift
+++ Healthub/View/Login/WelcomeView.swift
@@ -10,34 +10,32 @@ struct WelcomeView: View {
                     .resizable()
                     .scaledToFit()
                     .frame(width: 150)
-                VStack{
-                    Text("Hello")
-                        .font(.largeTitle.bold())
-                    Text("Welcome to Healthub")
-                        .foregroundColor(Color(.systemGray))
-                        .font(.system(size: 17, weight: .bold))
-                        .padding(.bottom, 5)
-                    HStack(spacing: 0){
-                        Text("Are you a doctor? Please ")
-                            .foregroundColor(Color(.systemGray3))
-                        NavigationLink(destination : DoctorView()){
-                            Text("Sign Here")
-                                .foregroundColor(Color(.systemPink))
-                        }
-                    }
-                    .font(.system(size: 15, weight: .bold))
-                }
+                Text("Hello")
+                    .font(.largeTitle.bold())
+                Text("Welcome to Healthub")
+                    .foregroundColor(Color(.systemGray))
+                    .font(.system(size: 17, weight: .bold))
+                    .padding(.bottom, 5)
                 Spacer()
+                
                 NavigationLink(destination: LoginView()){
                     Text("\(Image(systemName: "envelope")) Continue with Email")
-                        .foregroundColor(Color(.white))
-                        .font(.system(size: 15, weight: .medium))
-                        .padding(EdgeInsets(top: 12, leading: 24, bottom: 12, trailing: 24))
-                        .background(
-                            RoundedRectangle(cornerRadius: 10)
-                                .foregroundColor(Color(.systemPink)))
-                        .padding(.bottom,20)
+                        .font(.system(size: 15, weight: .semibold))
+                        .padding(EdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 10))
                 }
+                .buttonStyle(.borderedProminent)
+                .padding(.bottom,20)
+                
+                HStack(spacing: 0){
+                    Text("Are you a doctor? Please ")
+                        .foregroundColor(Color(.systemGray3))
+                    NavigationLink(destination : DoctorView()){
+                        Text("Sign Here")
+                            .foregroundColor(Color(.systemPink))
+                    }
+                }
+                .font(.system(size: 15, weight: .bold))
+                .padding(.bottom,20)
             }
         }
     }
