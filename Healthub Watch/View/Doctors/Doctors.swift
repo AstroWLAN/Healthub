4607c00b8f945c35160c5a8de4a5d8eb881cf77a
AstroWLAN <dario2.crippa@mail.polimi.it>
2022-12-24 16:15:43 +0100

    Adds doctors on the AW

--- Healthub Watch/View/Doctors/Doctors.swift
+++ Healthub Watch/View/Doctors/Doctors.swift
@@ -1,15 +1,38 @@
-//
-//  Doctors.swift
-//  Healthub Watch
-//
-//  Created by Dario Crippa on 21/12/22.
-//
-
 import SwiftUI
 
+struct Doctor: Hashable {
+    let name: String
+    let address: String
+}
+
+
 struct Doctors: View {
+    
+    @State private var userDoctors : [Doctor] = [
+        Doctor(name: "Shaun Murphy", address: "Via Della Spiga, 97"),
+        Doctor(name: "Gregory House", address: "Via Vicodin, 69")
+    ]
+    
     var body: some View {
-        Text("Hello, Doctors!")
+        List {
+            if userDoctors.isEmpty {
+                // Doctors Placeholder
+            }
+            else {
+                ForEach(userDoctors, id: \.self) { doctor in
+                    VStack(alignment: .leading) {
+                        Text(doctor.name)
+                            .font(.system(size: 17, weight: .semibold))
+                        Text(doctor.address)
+                            .font(.system(size: 15))
+                            .foregroundColor(.gray)
+                    }
+                }
+            }
+        }
+        .listStyle(.elliptical)
+        .lineLimit(1)
+        .minimumScaleFactor(0.2)
     }
 }
 
