370da804c4df1ad2d79219ffdbff9488ce2cf6db
Giovanni Dispoto <giovanni.dispoto@mail.polimi.it>
2023-01-26 10:22:57 +0100

    Reorganize tests + Add fetch all togheter

--- Healthub/View/Main.swift
+++ Healthub/View/Main.swift
@@ -3,6 +3,12 @@ import SwiftUI
 struct MainView: View {
     
     @EnvironmentObject private var loginViewModel: LoginViewModel
+    @EnvironmentObject private var ticketViewModel: TicketViewModel
+    @EnvironmentObject private var therapyViewModel: TherapyViewModel
+    @EnvironmentObject private var contactViewModel : ContactViewModel
+    @EnvironmentObject private var profileViewModel : ProfileViewModel
+    @EnvironmentObject private var pathologiesViewModel : PathologyViewModel
+    
     @AppStorage("firstAppLaunch") var firstAppLaunch : Bool = false
     @AppStorage("isLogged") var isLogged : Bool = false
     
@@ -25,7 +31,16 @@ struct MainView: View {
                 // Changes the tint of the tab bar
                 .tint(Color("AstroGray"))
             }
+            .onAppear(perform: {
+                ticketViewModel.fetchTickets(force_reload: true)
+                therapyViewModel.fetchTherapies(force_reload: true)
+                contactViewModel.fetchContacts(force_reload: true)
+                profileViewModel.getPatient(force_reload: true)
+                pathologiesViewModel.fetchPathologies(force_reload: true)
+                
+            })
             .sheet(isPresented: $firstAppLaunch) { OnBoardingView() }
+            
         }
     }
 }
